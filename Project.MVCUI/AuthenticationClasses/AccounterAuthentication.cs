9c8b4a424c91ee58c8f8a3573cd25c52bb3f7723
Eren Bektaş <Erenbektas95@hotmail.com>
2022-10-29 14:28:31 +0300

    VMclaslarını açtım

--- Project.MVCUI/AuthenticationClasses/AccounterAuthentication.cs
+++ Project.MVCUI/AuthenticationClasses/AccounterAuthentication.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Project.MVCUI.AuthenticationClasses
 {
-    public class AccounterAuthentication
+    public class AccounterAuthentication: AuthorizeAttribute
     {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Session["accounter"]!=null)
+            {
+                return true;
+            }
+            httpContext.Response.Redirect("/HomeLogin");
+            return false;
+        }
     }
 }
